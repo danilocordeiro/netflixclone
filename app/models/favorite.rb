328405d279a65b7f1d6ddc64714369d16f7724f2
Danilo Cordeiro <danilocordeiro.developer@gmail.com>
2018-05-23 19:56:15 -0300

    setting models and validation added

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,4 +1,5 @@
 class Favorite < ApplicationRecord
   belongs_to :favoriteable, polymorphic: true
   belongs_to :user
+  validates :user_id, uniqueness: { scope: [:favoriteable_type, :favoriteable_id], message: "can favorite only one time per resource" }
 end
